666e86854293d21886478937c9b57f789b1e185b
Tyuiu.LoginovMV@outlook.com <maximloginov22@gmail.com>
2023-09-22 22:36:28 +0500

    Task3.1

---
+++ Tyuiu.LoginovMV.Sprint1.Task3.V19.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.LoginovMV.Sprint1.Task3.V19.Lib;
+
+namespace Tyuiu.LoginovMV.Sprint1.Task3.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x1 = 2;
+            double x2 = 1;
+            double y1 = 2;
+            double y2 = 1;
+            var res = ds.ElephCanMove(x1, x2, y1, y2);
+            Assert.AreEqual(true, res);
+        }
+    }
+}
---
+++ Tyuiu.LoginovMV.Sprint1.Task3.V19/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.LoginovMV.Sprint1.Task3.V19.Lib;
+
+namespace Tyuiu.LoginovMV.Sprint1.Task3.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1| Выполнил: Логинов М.В | ИИПб-23-2";
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                                                  *");
+            Console.WriteLine("* Тема: Операторы составного присвоения                                                                      *");
+            Console.WriteLine("* Задание #3                                                                                                 *");
+            Console.WriteLine("* Вариант #19                                                                                                *");
+            Console.WriteLine("* Выполнил: Логинов М.В   | ИИПб-23-2                                                                        *");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                                   *");
+            Console.WriteLine("* Написать программу, которая печатает true или false в зависимости от того,                                 *");
+            Console.WriteLine("* может ли шахматная фигура «Слон» с одного заданного поля шахматной доски перейти за один ход на другое.    *");
+            Console.WriteLine("* Пользователь задаёт координаты двух ячеек шахматной доски(x1 и y1, x2 и y2, каждое в диапазоне от 1 до 8). *");
+            Console.WriteLine("*                                                                                                            *");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                           *");
+            Console.WriteLine("**************************************************************************************************************");
+
+            double x1;
+            Console.WriteLine("Введите координату Слона на оси  Ox =");
+            x1 = Convert.ToDouble(Console.ReadLine());
+
+            double y1;
+            Console.WriteLine("Введите координату Слона на оси  Oy =");
+            y1 = Convert.ToDouble(Console.ReadLine());
+
+            double x2;
+            Console.WriteLine("Введите координату поля, на которое должен за один ход перейти Cлон, на оси  Ox =");
+            x2 = Convert.ToDouble(Console.ReadLine());
+
+            double y2;
+            Console.WriteLine("Введите координату поля, на которое должен за один ход перейти Cлон, на оси  Oy =");
+            y2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                 *");
+            Console.WriteLine("**************************************************************************************************************");
+
+            Console.WriteLine(ds.ElephCanMove(x1, x2, y1, y2));
+            Console.ReadKey();
+        }
+    }
+}
