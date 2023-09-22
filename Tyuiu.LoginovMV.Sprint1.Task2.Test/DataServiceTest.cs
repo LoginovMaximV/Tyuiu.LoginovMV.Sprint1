66a21bae951cbc00dee4b763d6f63644f1908a48
Tyuiu.LoginovMV@outlook.com <maximloginov22@gmail.com>
2023-09-22 21:46:09 +0500

    Task3

---
+++ Tyuiu.LoginovMV.Sprint1.Task2.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.LoginovMV.Sprint1.Task3;
+using Tyuiu.LoginovMV.Sprint1.Task3.Lib;
+namespace Tyuiu.LoginovMV.Sprint1.Task3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x1 = 1;
+            double x2 = 2;
+            double y1 = 1;
+            double y2 = 2;
+            var res = ds.ElephCanMove(x1, x2, y1, y2);
+            Assert.AreEqual(true, res);
+        }
+    }
+}
