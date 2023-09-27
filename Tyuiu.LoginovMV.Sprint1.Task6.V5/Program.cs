6a92a3fe8e0c1cca4622761cd48f015cbbb60ec5
Максим Логинов <maxdompc@gmail.com>
2023-09-27 21:03:37 +0500

    Task6

---
+++ Tyuiu.LoginovMV.Sprint1.Task6.V5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.LoginovMV.Sprint1.Task6.V5.Lib;
+
+namespace Tyuiu.LoginovMV.Sprint1.Task6.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1| Выполнил: Логинов М.В | ИИПб-23-2";
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                                                  *");
+            Console.WriteLine("* Тема: Работа со строками класс String                                                                      *");
+            Console.WriteLine("* Задание #6                                                                                                 *");
+            Console.WriteLine("* Вариант #5                                                                                                 *");
+            Console.WriteLine("* Выполнил: Логинов М.В   | ИИПб-23-2                                                                        *");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                                   *");
+            Console.WriteLine("* Написать программу: пользователь вводит текст.                                                             *");
+            Console.WriteLine("* Напечатать те слова, которые являются симметричными (например: казак, шалаш).                              *");
+            Console.WriteLine("*                                                                                                            *");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                           *");
+            Console.WriteLine("**************************************************************************************************************");
+            string x;
+            Console.WriteLine("Введите предложение:");
+            x = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                 *");
+            Console.WriteLine("**************************************************************************************************************");
+            string[] xs = x.Split(' ');
+            for (int i = 0; i < xs.Length; i++)
+            {
+                Console.WriteLine(ds.CheckSymmetricalWords(xs[i]));
+            }
+            Console.ReadKey();
+        }
+    }
+}
